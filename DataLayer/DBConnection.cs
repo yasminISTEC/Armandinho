9c4f0b485515af80307abfb45f19f0c93baef600
Yasmin Dias <yasmin.cruz.2025072@my.istec.pt>
2026-06-13 12:24:34 +0100

    Add project files.

---
+++ DataLayer/DBConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace DataLayer
+{
+    internal class DBConnection
+        
+    {
+        privatestatic string connectionString = "Data Source=localhost;Initial Catalog=REDACTED_DB_NAME;Integrated Security=True";
+    }
+}
