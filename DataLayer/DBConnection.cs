052546ced8948a259418f5bf469cad7614f320a6
Yasmin Dias <yasmin.cruz.2025072@my.istec.pt>
2026-06-17 20:46:48 +0100

    Criando query para a tabela Livros-- oq foi feito:  Apenas a parte de listar todos os livros

--- DataLayer/DBConnection.cs
+++ DataLayer/DBConnection.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
 namespace DataLayer
 {
-    internal class DBConnection
+    public class DBConnection
         
     {
-        privatestatic string connectionString = "Data Source=localhost;Initial Catalog=REDACTED_DB_NAME;Integrated Security=True";
+        private static string connectionString = "Data Source=localhost;Initial Catalog=REDACTED_DB_NAME;Integrated Security=True";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
     }
 }
---
+++ DataLayer/LivrosData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+
+namespace DataLayer
+{
+    public class LivrosData
+    {
+        public List<Livro> ListarLivros()
+        {
+            List<Livro> livros = new List<Livro>();
+
+            using (SqlConnection con = DBConnection.GetConnection())
+            {
+                string query = "SELECT * FROM Livros";
+                SqlCommand cmd = new SqlCommand(query, con);
+                con.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    livros.Add(new Livro
+                    {
+                        IdLivro = (int)reader["IdLivro"],
+                        ISBN = reader["ISBN"].ToString(),
+                        Titulo = reader["Titulo"].ToString(),
+                        Autor = reader["Autor"].ToString(),
+                        AnoPublicaco = reader["AnoPublicacao"] == DBNull.Value ? null : (int?)reader["AnoPublicacao"],
+                        ExemplaresDisponiveis = (int)reader["ExemplaresDisponiveis"]
+                    });
+                }
+            }
+            return livros;
+        }
+
+    }
+}
