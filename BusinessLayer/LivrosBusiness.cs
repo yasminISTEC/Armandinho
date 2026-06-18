061b1ef02ea87b198d612ea12dabf829f4309943
Yasmin Dias <yasmin.cruz.2025072@my.istec.pt>
2026-06-18 11:33:24 +0100

    refazendo

---
+++ BusinessLayer/LivrosBusiness.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class LivrosBusiness
+    {
+        private LivrosData _livrosData = new LivrosData();
+
+        //validar livro
+
+        public string ValidarLivro(Livro livro)
+        {
+            if (string.IsNullOrWhiteSpace(livro.ISBN))
+                return "O ISBN 'e obrigatorio";
+
+            if (livro.ISBN.Length > 20)
+                return "O ISBN nao pode ter mais de 20 caracteres";
+
+            if (string.IsNullOrWhiteSpace(livro.Titulo))
+                return "O Titulo 'e obrigatorio";
+
+            if (string.IsNullOrEmpty(livro.Autor))
+                return "O Autor 'e obrigatorio";
+
+            if (livro.AnoPublicacao.HasValue)
+            {
+                if (livro.AnoPublicacao < 1000 || livro.AnoPublicacao > DateTime.Now.Year)
+                    return $"O ano deve ser entre 1000 e {DateTime.Now.Year}";
+
+                if (livro.ExemplaresDisponiveis < 0)
+                    return "Os exemplares nao podem ser negativos";
+            }
+            return null;
+        }
+
+        //inserir com vaidacoes
+
+        public string InserirLivro(Livro livro)
+        {
+            string erro = ValidarLivro(livro);
+            if (erro != null)
+                return erro;
+            
+            _livrosData.InserirLivro(livro);
+            return null;  
+
+        }
+
+        //ediar com validacaoes
+
+        public string EditarLivro(Livro livro)
+        {
+            string erro = ValidarLivro(livro);
+            if (erro != null) return erro;
+
+            _livrosData.EditarLivro(livro);
+            return null;
+        }
+    }
+}
--- InterfaceLayer/Livro.cs
+++ InterfaceLayer/Livro.cs
@@ -11,7 +11,7 @@ namespace DataLayer
         public string Titulo { get; set; }
         public string Autor { get; set; }
 
-        public int? AnoPublicacao { get; set; }
+        public int? AnoPublicaco { get; set; }
 
         public int ExemplaresDisponiveis { get; set; }
     }
